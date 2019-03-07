742655cd685715fa8302016dab482ff2f40c8aa5
chenwx <chenwx716@163.com>
2019-03-08 07:49:03 +0800

    build.sh

--- build.sh
+++ build.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
 # 0.1.2         2019-01-11 15:09:56
+work_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 version=0.1.2
-img_dir=/data/share/docker_images
-container=bmgrweb
 
-img_file=$img_dir/chenwx_${container}_${version}.tar
+container=bmgrweb
 
 docker stop $container
 docker rm $container
 docker rmi chenwx/$container:$version
-rm $img_file
-
-#------------------------------------------------
 
 tar zcf bmgrweb.tar.gz ./dist/
 
-docker build -t chenwx/$container:$version -f dockerfile .
+docker build -t chenwx/$container:$version -f $work_dir/dockerfile .
 
 rm bmgrweb.tar.gz
-
-docker save chenwx/$container:$version > $img_file
 
 docker run --name $container -h $container --net="host" -d chenwx/$container:$version
