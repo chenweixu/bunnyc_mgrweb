7133889e438f74b60aa71d1204028144d1c62fe1
chenwx <chenwx716@163.com>
2019-05-06 11:01:32 +0800

    remotecmd add user

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # 0.1.3         2019-04-21 08:42:59
+# 0.1.4         2019-05-06 08:49:43
+
 work_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-version=0.1.3
+version=0.1.4
 
 container=bmgrweb
 img_dir=/data/share/docker_images
@@ -21,7 +23,7 @@ docker build -t chenwx/$container:$version -f $work_dir/dockerfile .
 rm bmgrweb.tar.gz
 
 docker run --name $container -h $container --net="host" -d chenwx/$container:$version
-# docker run --name bmgrweb -h bmgrweb --net="host" -d chenwx/bmgrweb:0.1.3
+# docker run --name bmgrweb -h bmgrweb --net="host" -d chenwx/bmgrweb:0.1.4
 
 docker save chenwx/$container:$version > $img_dir/chenwx_${container}_${version}.tar
 gzip $img_dir/chenwx_${container}_${version}.tar
--- src/components/host/remotecmd.vue
+++ src/components/host/remotecmd.vue
@@ -5,15 +5,20 @@
   <el-tabs v-model="activeName" type="border-card" @tab-click="handleClick">
     <el-tab-pane label="单机任务" name="single">
     <el-row :gutter="5" :span="5">
-      <el-col :span="6">
+      <el-col :span="4">
         <el-input v-model="ip" placeholder="请输入ipv4 地址" clearable></el-input>
       </el-col>
 
-      <el-col :span="6">
+      <el-col :span="8">
         <el-input v-model="cmd" placeholder="命令" clearable></el-input>
       </el-col>
 
-      <el-col :span="6">
+      <el-col :span="3">
+        <el-input v-model="user" placeholder="用户名" clearable></el-input>
+      </el-col>
+
+
+      <el-col :span="2">
         <el-button type="primary" v-on:click="cmdManager()">提交</el-button>
       </el-col>
     </el-row>
@@ -49,6 +54,7 @@
         activeName: 'single',
         ip: '',
         cmd: '',
+        user: '',
       }
     },
     methods: {
@@ -65,6 +71,7 @@
                 "task": "remote",
                 "ip": this.ip,
                 "cmd": this.cmd,
+                "user": this.user
                 }
         };
 
@@ -73,16 +80,21 @@
         .then((res) => {
           console.log(res.data)
           if (res.status == 200) {
-            this.$notify({type: 'success', title: '成功',message: '执行成功'});
-            this.displayData = true;
-            this.rundata = '<pre>'+res.data.redata+'</pre>';
+            if (res.data.recode == 0) {
+              this.$notify({type: 'success', title: '成功',message: '执行成功'});
+              this.displayData = true;
+              this.rundata = '<pre>'+res.data.redata+'</pre>';
             } else {
-              this.$notify({type: 'error', title: '失败',message: '执行失败'});
+              this.$notify({type: 'error', title: '失败',message: res.data.redata});
             }
+
+          } else {
+            this.$notify({type: 'error', title: '失败',message: '请求失败'});
+          }
         })
         .catch((error) => {
           // 这里是处理错误的回调
-          this.$notify({type: 'error', title: '失败',message: '操作失败'});
+          this.$notify({type: 'error', title: '失败',message: '请求失败'});
           console.log(error);
         });
       },
